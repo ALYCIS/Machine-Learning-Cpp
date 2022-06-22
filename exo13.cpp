ad19cbad6ccb0f509c6ee681a8a0f94b15a26af0
CISSE ALY <alyhabcis@gmail.com>
2022-06-22 16:03:23 +0200

    Version Finale du programme qui retourne le ieme bit d'un entier non signé sur 32 bits

--- exo13.cpp
+++ exo13.cpp
@@ -12,22 +12,10 @@ public:
   static int getBit(unsigned int value, int pos)
   {
     std::bitset<32> value_binaire(value);
-
-    // cout<<"Bitset de :"<<value<<endl;
-    // for(int i = 0; i <32 ; i++)
-    //   {
-    //     cout<<value_binaire[31-i];
-    //   }
-    // cout<<endl;
-
     return value_binaire[pos];
   }
 
 };
-
-
-
-
 
 
 int main(){
